acd8bf817676c45f6870fc589713c2329f864cb6
Ayush23Dash <ayushshanker23@gmail.com>
2020-03-24 18:31:19 +0530

    Added basic functionality

---
+++ app.js
@@ -0,0 +1,41 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const rp = require('request-promise');
+const cheerio = require('cheerio'); // Basically jQuery for node.js
+
+let app = express();
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+let c = new Array();
+
+rp('https://www.mohfw.gov.in/',function(err,res,body){
+  let $ = cheerio.load(body);
+  let a = $('.icount');
+  for(let i =0;i<5;i++){
+  let b = a[i];
+
+  c.push(b.children[0].data);
+  // c = (b.children[0].data);
+
+  console.log(c);
+
+}
+
+});
+app.get("/", (req,res) =>{
+
+  res.render("home",{value :c});
+
+});
+
+
+app.listen(3000,() => {
+console.log("Server is running on port 3000");
+}
+);
