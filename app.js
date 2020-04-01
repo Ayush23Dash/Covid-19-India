1a27b9727006019562dad607fd2ba68955de6285
Ayush23Dash <ayushshanker23@gmail.com>
2020-04-01 13:00:02 +0530

    Corrected newly updated scraping source

--- app.js
+++ app.js
@@ -16,14 +16,22 @@ let c = new Array();
 
 rp('https://www.mohfw.gov.in/',function(err,res,body){
   let $ = cheerio.load(body);
-  let a = $('.icount');
-  for(let i =0;i<5;i++){
-  let b = a[i];
+  // let a = $('.icount');
+  // for(let i =0;i<5;i++){
+  // let b = a[i];
 
-  c.push(b.children[0].data);
-}
-
+  // c.push(b.children[0].data);
+  let a = $('.bg-blue');
+  let b = $('.bg-green');
+  let d = $('.bg-red');
+  let e = $('.bg-orange');
+  c.push(a[0].children[0].parent.children[3].children[0].data);
+  c.push(b[0].children[0].parent.children[3].children[0].data);
+  c.push(d[0].children[0].parent.children[3].children[0].data);
+  c.push(e[0].children[0].parent.children[3].children[0].data);
 });
+
+// });
 // AQI
     let aqi,condition;
 rp('https://aqicn.org/city/delhi/',function(err,res,body)
